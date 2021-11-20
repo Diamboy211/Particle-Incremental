3ed3fbc8a1df4ad43aa6e6751784162cd28b0508
Diamboy211 <51283163+Diamboy211@users.noreply.github.com>
2021-11-20 17:22:52 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,37 @@
-let x = 0
-let y = 1
-let uprunning = 0
-
-function upfunction() {
-    setInterval(() => {
-        x = x + y
-        document.getElementById("counter").textContent = x
-        uprunning = 1
-    }, 1000);
-}
-
+let game_data = {
+    x: 0,
+    y: 0,
+    running: false,
+    last_tick: null,
+    time_until_increment: 0.001;
+};
 
 function callup() {
-    if(uprunning == 0) {
-      upfunction()  
-    }
+    game_data.running = !game_data.running;
 }
 
 function incy() {
-    y++
+    game_data.y++
 }
+
+function main() {
+    game_data.last_tick = Date.now();
+    loop();    
+}
+
+function loop() {
+    requestAnimationFrame(loop);
+    const _dn = Date.now();
+    const dt = (_dn - game_data.last_tick) * 0.001;
+    game_data.last_tick = _dn;
+    
+    if (game_data.is_running) {
+        game_data.time_until_increment -= dt;
+        if (game_data.time_until_increment <= 0) {
+            game_data.x += game_data.y;
+            document.getElementById("counter").textContent = x;
+        }
+    }
+}
+
+main();
