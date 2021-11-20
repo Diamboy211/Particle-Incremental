79cbad4cc5c682c3af432c35dbb03813bfa7c2aa
Diamboy211 <51283163+Diamboy211@users.noreply.github.com>
2021-11-20 20:56:22 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ let game_data = {
     y: 0,
     running: false,
     last_tick: null,
-    time_until_increment: 0.001,
 };
 
 function callup() {
@@ -25,19 +24,8 @@ function loop() {
     const _dn = Date.now();
     const dt = (_dn - game_data.last_tick) * 0.001;
     game_data.last_tick = _dn;
-    
-    if (game_data.running) {
-        game_data.time_until_increment -= dt;
-        let tick_rate = Math.max(1, Math.min(game_data.y, 20));
-        if (game_data.time_until_increment <= 0) {
-            game_data.time_until_increment += 1 / tick_rate;
-            game_data.x += game_data.y / tick_rate;
-            document.getElementById("counter").textContent = Math.floor(game_data.x);
-        }
-        if (game_data.time_until_increment < 0)
-            document.getElementById("lag").textContent = `${-Math.floor(game_data.time_until_increment)} frames behind`;
-        else document.getElementById("lag").textContent = "";
-    }
+    game_data.x += game_data.y * dt;
+    document.getElementById("counter") = Math.floor(game_data.x);
 }
 
 main();
