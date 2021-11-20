442738009ec1feb28c0d56c9b79130e4b61ce943
Diamboy211 <diamboy212@gmail.com>
2021-11-20 21:05:53 +0700

    changed spaces to tabs and crlf to lf

--- main.js
+++ main.js
@@ -1,31 +1,31 @@
 let game_data = {
-    x: 0,
-    y: 0,
-    running: false,
-    last_tick: null,
+	x: 0,
+	y: 0,
+	running: false,
+	last_tick: null,
 };
 
 function callup() {
-    game_data.running = !game_data.running;
-    document.getElementById("generator").textContent = `generator ${game_data.running ? "on" : "off"}`;
+	game_data.running = !game_data.running;
+	document.getElementById("generator").textContent = `generator ${game_data.running ? "on" : "off"}`;
 }
 
 function incy() {
-    game_data.y++
+	game_data.y++;
 }
 
 function main() {
-    game_data.last_tick = Date.now();
-    loop();    
+	game_data.last_tick = Date.now();
+	loop();    
 }
 
 function loop() {
-    requestAnimationFrame(loop);
-    const _dn = Date.now();
-    const dt = (_dn - game_data.last_tick) * 0.001;
-    game_data.last_tick = _dn;
-    game_data.x += game_data.y * dt;
-    document.getElementById("counter").textContent = Math.floor(game_data.x);
+	requestAnimationFrame(loop);
+	const _dn = Date.now();
+	const dt = (_dn - game_data.last_tick) * 0.001;
+	game_data.last_tick = _dn;
+	game_data.x += game_data.y * dt;
+	document.getElementById("counter").textContent = Math.floor(game_data.x);
 }
 
 main();
